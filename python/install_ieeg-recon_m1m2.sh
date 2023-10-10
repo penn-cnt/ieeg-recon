4c0f0f52b07b0a8e9e3a45b7ac31cb038d315168
allucas <alfredo1238@gmail.com>
2023-10-10 12:24:48 -0400

    Included script to install iEEG-recon in M1/M2

---
+++ python/install_ieeg-recon_m1m2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Create a conda environment with the specified Python version
+conda create -n ieeg_recon_m1 python=3.9 -y
+
+# Activate the conda environment
+source activate ieeg_recon_m1
+
+# Upgrade pip
+pip install -U pip
+
+# Install the required Python packages
+pip install antspyx
+pip install antspynet
+
+# Uninstall tensorflow
+pip uninstall tensorflow -y
+
+# Install tensorflow for macOS with metal support
+SYSTEM_VERSION_COMPAT=0 pip install tensorflow-macos tensorflow-metal
+
+# Install a specific version of tensorflow_probability
+pip install tensorflow_probability==0.20.0
+
+# Install the rest of the python packages
+pip install nipype
+pip install niworkflows
+pip install ipython
+
+echo "Python Environment installed..."
+echo "Activate the environment with: conda activate ieeg_recon_m1"
+echo "After activation, run iEEG-recon"
